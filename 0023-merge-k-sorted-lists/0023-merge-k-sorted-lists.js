659138d92741fd4d47333354d49bf6a22b0ab2f6
Benjamin Wilson <122393808+BenjaminWilson13@users.noreply.github.com>
2023-08-24 14:49:05 -0700

    Time: 87 ms (70.87%), Space: 49.9 MB (19.13%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    const array = []; 
+    for (let list of lists) {
+        while (list) {
+            array.push(list.val); 
+            list = list.next; 
+        }
+    }
+    console.log(array);
+    if (!array.length) return null;  
+    sortedArray = array.sort((a, b) => a - b); 
+
+    const head = new ListNode(sortedArray[0]); 
+    let node = head; 
+    for (let i = 1; i < sortedArray.length; i++) {
+        node.next = new ListNode(sortedArray[i])
+        node = node.next; 
+    }
+    return head; 
+};
