c2aa4f61a15aae3c4ae22e6775e1d63ca82df820
Benjamin Wilson <122393808+BenjaminWilson13@users.noreply.github.com>
2023-10-22 11:37:39 -0500

    Time: 107 ms (23.65%), Space: 44.7 MB (48.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,27 +3,11 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    // console.log(nums)
-    // let count = 0
-    // for (let x = 0; x < nums.length - 1; x++) {
-    //     if (nums[x] === nums[x + 1] && nums[x] !== null) {
-    //         for (let y = x + 1; y < nums.length - 1; y++) {
-    //             nums[y] = nums[y + 1]
-    //         }
-    //         console.log(nums) 
-    //         nums[nums.length - 1] = null; 
-    //         count++; 
-    //         x--; 
-    //     }
-    // }
-    // console.log(count)
-    // return nums.length - count
-
-    const test = new Set(nums)
-    let i = 0; 
-    test.forEach(num => {
-        nums[i] = num; 
-        i++; 
-    })
-    return test.size; 
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1); 
+            i--; 
+        }
+    }
+    return nums.length; 
 };
