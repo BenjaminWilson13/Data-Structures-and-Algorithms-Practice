5cd2b7bd3875717f225e7ba0344b08828cde7ff2
Benjamin Wilson <122393808+BenjaminWilson13@users.noreply.github.com>
2023-10-22 12:23:00 -0500

    Time: 126 ms (9.54%), Space: 45 MB (22.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    for (let i = 0; i < nums.length - 1; i++) {
+    for (let i = 0; i < nums.length; i++) {
         if (nums[i] === nums[i + 1]) {
             nums.splice(i, 1); 
             i--; 
